a81cb14baa4b854177115041e4b4ffa56f996e43
wei2learn <wei2learn@gmail.com>
2016-05-26 21:14:47 -0400

    initial commit

---
+++ app/pages/map/map.ts
@@ -0,0 +1,62 @@
+import {Page} from 'ionic-angular';
+
+@Page({
+  templateUrl: 'build/pages/map/map.html'
+})
+export class HomePage {
+  map;
+  constructor() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let options = { timeout: 10000, enableHighAccuracy: true };
+
+    navigator.geolocation.getCurrentPosition(
+
+      (position) => {
+        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      },
+
+      (error) => {
+        console.log(error);
+      }, options
+
+    );
+  }
+
+
+  addMarker() {
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+
+  }
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
+}
