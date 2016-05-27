e09cfbe91aa4072c6ef09edebfd68204f81f650f
Wei Zhou <wei2learn@gmail.com>
2016-05-27 00:39:26 -0400

    can now map to address

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -5,34 +5,44 @@ import {Page} from 'ionic-angular';
 })
 export class HomePage {
   map;
+  geocoder;
+
   constructor() {
-    this.loadMap();
+    this.geocoder = new google.maps.Geocoder();
+    var address = 'edmonton, ab, canada';
+    this.mapAddress(address);
   }
 
-  loadMap() {
-    let options = { timeout: 10000, enableHighAccuracy: true };
+  mapAddress(address: string) {
+    this.geocoder.geocode({ 'address': address }, (results, status) => {
+      if (status == google.maps.GeocoderStatus.OK) {
+        this.mapLocation(results[0].geometry.location);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
 
-    navigator.geolocation.getCurrentPosition(
-
-      (position) => {
-        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-        let mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-
-        this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-      },
-
-      (error) => {
-        console.log(error);
-      }, options
-
-    );
   }
 
+  mapLocation = (position: google.maps.LatLng) => {
+    {
+
+      let mapOptions = {
+        center: position,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      // this.map.setCenter(position);
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position
+      });
+    }
+
+  }
 
   addMarker() {
 
